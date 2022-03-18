ece3c4520debe8971a18e4020b7480846017d9f2
Roy Yang <roy82859370@gmail.com>
2022-03-19 01:31:02 +0800

    first commit

---
+++ products.py
@@ -0,0 +1,15 @@
+#製作及理解二維清單
+product = []
+while True :
+	name = input('請輸入商品名稱:')
+	if name == 'q' :
+		break
+	price = input('請輸入商品價格:')
+	p=[]
+	p.append(name)
+	p.append(price)
+	product.append(p)      #8~11行=products.append([name],[price])
+print(product)
+print(product[0][1])
+
+
