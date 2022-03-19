2c9989f387e3b5e9eed23235a141ff3ac4582991
Roy Yang <roy82859370@gmail.com>
2022-03-19 11:19:04 +0800

    fourth commit

--- products.py
+++ products.py
@@ -15,6 +15,7 @@ print(product[0][1])
 for f in product :
 	print(f[0],'的價格為',f[1])
 
-with open('products.csv','w') as s :
+with open('products.csv','w',encoding='utf-8') as s :
+	s.write('商品,價格\n')
 	for g in product :
 		s.write(g[0]+','+g[1]+'\n')
