6f051744d846d74b969c77f26a1842f3c79918b9
Roy Yang <roy82859370@gmail.com>
2022-03-24 23:08:23 +0800

    lesson

---
+++ dict.py
@@ -0,0 +1,19 @@
+words = {'mom': '媽媽','dad': '爸爸'}
+
+print(words['mom'])
+print(words['dad'])
+
+words['tea'] = '茶'
+print(words['tea']) 
+
+p0 = {
+	'name':'麥香奶茶',
+	'price' : 10
+}
+
+p1 = {
+	'name':'珍珠奶茶',
+	'price' : 60
+}
+drinks = [p0,p1]
+print(drinks[0]['name'])
