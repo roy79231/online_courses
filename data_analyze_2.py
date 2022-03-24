283d58f0a2e12393eec8ed617a5efb78f4db8413
Roy Yang <roy82859370@gmail.com>
2022-03-24 23:57:35 +0800

    add progressbar

--- data_analyze_2.py
+++ data_analyze_2.py
@@ -1,16 +1,17 @@
 #數據分析--學會如何分析數據和從中篩出自己想要的
-#因為數據檔案過大，因此改用文字檔(txt)傳
-
+import time
+import progressbar
 data=[]
 count = 0
+bar = progressbar.ProgressBar(max_value = 1000000)
 with open('reviews.txt','r' ) as f :
 	for line in f:
 		data.append(line)
 		count += 1 #count = count +1
-		if count % 1000 == 0: # %為餘數
-			print(len(data))
+		bar.update(count)		
 print('檔案讀取完畢，共有',len(data),'筆資料')
 
+start = time.time()
 wc = {}             #word_count
 for line in data : 
 	words = line.split(' ')
@@ -19,6 +20,8 @@ for line in data :
 			wc[word] += 1
 		else :
 			wc[word] = 1
+end = time.time()
+print('花費',end-start,'秒')
 
 while True :
 	x = input('請輸入你想找出現幾次的單字:')
